52227b5f23cad3cf7d6d10f18ed0c14da522ccc5
Antony <antony.cn@icloud.com>
2025-09-19 14:19:43 +0800

    Update vite.config.ts

--- client-extensions/frontend/vite.config.ts
+++ client-extensions/frontend/vite.config.ts
@@ -1,7 +1,48 @@
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { resolve } from 'path';
+import { glob } from 'glob';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
-// https://vitejs.dev/config/
+const outDir = resolve(__dirname, 'build-vite');
+
+const getEntries = () => {
+	const _getEntries = (filePathPattern: string | string[]) => {
+		return Object.fromEntries(
+			glob
+				.sync(filePathPattern)
+				.map(file => [
+					path.relative(
+						'src',
+						file.slice(0, file.length - path.extname(file).length)
+					),
+					fileURLToPath(new URL(file, import.meta.url)),
+				])
+		);
+	};
+
+	let entries = {
+		..._getEntries([
+			`src/apps/**/main.ts`,
+			`src/apps/**/style.scss`,
+			`src/global/**/*.js`,
+			`src/global/**/*.css`,
+		]),
+	};
+
+	//console.log(entries);
+	return entries;
+};
+
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+			outDir,
+			emptyOutDir: true,
+			rollupOptions: {
+				input: getEntries(),
+			},
+		},
 })
