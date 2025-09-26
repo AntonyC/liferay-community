13b2530cde93600d44a0cfaccff49489b17f1939
Antony <antony.cn@icloud.com>
2025-09-26 23:52:15 +0800

    refactor to vite

--- client-extensions/frontend/vite.config.ts
+++ client-extensions/frontend/vite.config.ts
@@ -1,48 +1,47 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react'
-import { resolve } from 'path';
-import { glob } from 'glob';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import { glob } from "glob";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
-const outDir = resolve(__dirname, 'build-vite');
+const outDir = resolve(__dirname, "build-vite");
 
 const getEntries = () => {
-	const _getEntries = (filePathPattern: string | string[]) => {
-		return Object.fromEntries(
-			glob
-				.sync(filePathPattern)
-				.map(file => [
-					path.relative(
-						'src',
-						file.slice(0, file.length - path.extname(file).length)
-					),
-					fileURLToPath(new URL(file, import.meta.url)),
-				])
-		);
-	};
+  const _getEntries = (filePathPattern: string | string[]) => {
+    return Object.fromEntries(
+      glob
+        .sync(filePathPattern)
+        .map((file) => [
+          path.relative(
+            "src",
+            file.slice(0, file.length - path.extname(file).length)
+          ),
+          fileURLToPath(new URL(file, import.meta.url)),
+        ])
+    );
+  };
 
-	let entries = {
-		..._getEntries([
-			`src/apps/**/main.ts`,
-			`src/apps/**/style.scss`,
-			`src/global/**/*.js`,
-			`src/global/**/*.css`,
-		]),
-	};
+  let entries = {
+    ..._getEntries([
+      `src/apps/**/main.ts`,
+      `src/apps/**/style.scss`,
+      `src/global/**/*.js`,
+      `src/global/**/*.css`,
+    ]),
+  };
 
-	//console.log(entries);
-	return entries;
+  //console.log(entries);
+  return entries;
 };
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-			outDir,
-			emptyOutDir: true,
-			rollupOptions: {
-				input: getEntries(),
-			},
-		},
-})
+  plugins: [
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler"]],
+      },
+    }),
+  ],
+});
