4da6606b8ca10781f5cb25ca90aed17a78f208a9
Antony <antony.cn@icloud.com>
2025-10-08 00:49:43 +0800

    add stake

--- client-extensions/frontend/src/apps/stake/App.tsx
+++ client-extensions/frontend/src/apps/stake/App.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { ClayIconSpriteContext } from "@clayui/icon";
+import { Header } from "../components";
 
 export default class App extends HTMLElement {
   connectedCallback() {
@@ -8,6 +9,7 @@ export default class App extends HTMLElement {
       <BrowserRouter>
         <ClayIconSpriteContext.Provider value={Liferay.Icons.spritemap}>
           <div>aaaaabbb</div>
+          <Header />
         </ClayIconSpriteContext.Provider>
       </BrowserRouter>
     );
